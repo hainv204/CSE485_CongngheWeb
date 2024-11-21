60823e51bf8fe261c815e84914a34230a1b90b4f
hainv204 <hainv07204@gmail.com>
2024-11-21 13:11:20 +0700

    update BT1

--- BT1/index.php
+++ BT1/index.php
@@ -3,24 +3,24 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Product</title>
+<title>Products</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" rel="stylesheet">
 </head>
 <body>
 <?php
  $products = [
-    ['name' => 'Sản phẩm 1', 'price' => 1000],
-    ['name' => 'Sản phẩm 2', 'price' => 2000],
-    ['name' => 'Sản phẩm 3', 'price' => 3000],
-    ['name' => 'Sản phẩm 4', 'price' => 5000],
-    ['name' => 'Sản phẩm 5', 'price' => 8000]
+    ['name' => 'product 1', 'price' => 1000],
+    ['name' => 'product 2', 'price' => 2000],
+    ['name' => 'product 3', 'price' => 3000],
+    ['name' => 'product 4', 'price' => 5000],
+    ['name' => 'product 5', 'price' => 8000]
  ];
  static $count=1;
 ?>
 <header>
 <div class="heading d-flex justify-content-between bg-dark">
-	<h2 class="text-light pt-3">List Product</h2>
+	<h2 class="text-light pt-3">List Products</h2>
 	<div class="form-button mx-4 my-3">
 		<button class="btn btn-danger">Delete</button>
 		<button class="btn btn-secondary">Add new product</button>
@@ -29,12 +29,14 @@
 </header>
 
 <main>
-<table class="table table-bordered table-striped table-hover">
+<table class="table table-bordered table-hover">
 	<thead>
 		<tr>
 			<th class="py-3 text-center">STT</th>
 			<th class="py-3 text-center">Name</th>
 			<th class="py-3 text-center">Price</th>
+			<th class="py-3 text-center">Update</th>
+			<th class="py-3 text-center">Delete</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -43,6 +45,8 @@
 				<td class="py-3 text-center"><?= htmlspecialchars($count++) ?></td>
 				<td class="py-3 text-center"><?= htmlspecialchars($value['name']) ?></td>
                 <td class="py-3 text-center"><?= htmlspecialchars($value['price']) ?> VND</td>
+				<td class="py-3 text-center"><i class="fa-solid fa-pen-to-square"></i></td>
+				<td class="py-3 text-center"><i class="fa-solid fa-trash"></i></td>
 			</tr>
 		<?php endforeach;?>
 	</tbody>
@@ -61,5 +65,6 @@
 		</ul>
 	</div>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
